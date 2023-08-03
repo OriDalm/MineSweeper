46593a7398507d5fbbcf616d78d72c355c7b2df4
OriDalm <141171920+OriDalm@users.noreply.github.com>
2023-08-03 20:56:21 +0300

    Add files via upload
    FINAL DELIVERY - Thursday

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
 'use strict'
-//Create MineSweeper
+//Create MineSweeper :)
 var gBoard
-const EMPTY = ''
-const MINE = '💣'
-var gStartTime = null;
-var gTimerInterval = null;
+var gInterval
+var gBestTime
+var Modes = true
+var gTime
+var gStartTime = null
+var gTimerInterval = null
 var gLevel = {
     SIZE: 4,
     MINES: 2
 }
-
 var gGame
-
-function createEmptyBoard() {
-
-}
-
-
-
+var gTimesLost = 0
+var gLifeCount = 3
 
 function onInit() {
+    gTimesLost = 0
+    gLifeCount = 3
+    gBestTime = parseInt(localStorage.getItem('bestTime')) || Infinity
+    document.getElementById('best-time').innerHTML = localStorage.getItem('bestTime')
 
-
-    clearInterval(gTimerInterval)
+    clearInterval(gInterval)
     var elBtn = document.querySelector('.reset')
     if (gLevel.SIZE === 4) {
         elBtn.innerHTML = `<button class="restart" onclick=chooseEasy()>😄</button>`
@@ -44,12 +43,10 @@ function onInit() {
     gGame.isOn = true
     gBoard = createBoard()
     renderBoard(gBoard)
+    flagCount()
     onCellMarked()
 
-
-
 }
-
 
 function createBoard() {
     const board = []
@@ -65,6 +62,7 @@ function createBoard() {
 
         }
     }
+
     return board
 }
 
@@ -81,16 +79,12 @@ function placeRandomMines(board) {
 
 }
 
-
-
-
 function renderBoard(board) {
     var strHTML = ''
     for (var i = 0; i < board.length; i++) {
         strHTML += `<tr>\n`
         for (var j = 0; j < board[i].length; j++) {
             const cell = board[i][j]
-            const classStr = cell.isShown ? 'str' : ''
             const showing = cell.isShown ? cell.minesAroundCount : ''
             strHTML += `\t<td class="str"
              onclick="onCellClicked(this, ${i}, ${j})"
@@ -122,27 +116,42 @@ function setMinesNegsCount(board, rowIdx, colIdx) {
 }
 
 function onCellClicked(elCell, rowIdx, colIdx) {
-
-
-
+    var boardSize = gLevel.SIZE * gLevel.SIZE
+    expandShown(gBoard, elCell, rowIdx, colIdx)
     if (!gGame.isOn || gBoard[rowIdx][colIdx].isShown || gBoard[rowIdx][colIdx].isMarked) return
-    if (gBoard[rowIdx][colIdx].isMine) {
-        elCell.innerText = '💣'
-        return onLose(gBoard, elCell, rowIdx, colIdx)
-    }
+
     var minesCount = setMinesNegsCount(gBoard, rowIdx, colIdx)
     elCell.innerText = minesCount
     gBoard[rowIdx][colIdx].isShown = true
+    if (gLifeCount > 1 && gBoard[rowIdx][colIdx].isMine) {
+        gLifeCount--
+        gTimesLost++
+        gGame.shownCount--
+        // console.log('Lifecount', gLifeCount);
+        elCell.innerText = '💔'
+    } else if (gLifeCount === 1 && gBoard[rowIdx][colIdx].isMine) {
+        elCell.innerText = '💣'
+
+        return onLose(gBoard, elCell, rowIdx, colIdx)
+    }
     gGame.shownCount++
-    console.log('showcount', gGame.shownCount);
-    console.log('markedcount', gGame.markedCount);
-    if (gGame.shownCount === gLevel.SIZE * gLevel.SIZE - gLevel.MINES && gGame.markedCount === gLevel.MINES) onWin()
+    lifeCount()
+    // console.log('size', boardSize);
+    // console.log('timeslost', gTimesLost);
+    // console.log('lifecount', gLifeCount);
+    // console.log('showcount', gGame.shownCount);
+    // console.log('markedcount', gGame.markedCount);
+    if (gGame.shownCount === boardSize - gLevel.MINES - gTimesLost
+         && gGame.markedCount + gTimesLost === gLevel.MINES 
+         || boardSize === gGame.shownCount + gGame.markedCount + gTimesLost) onWin()
     if (gGame.shownCount === 1 && gGame.markedCount === 0 || gGame.shownCount === 0 && gGame.markedCount === 1)
-        startTimer()
+        timer()
+    flagCount()
+
 }
 
-
 function onCellMarked(elCell, event, rowIdx, colIdx) {
+    var boardSize = gLevel.SIZE * gLevel.SIZE
 
     event.preventDefault()
     if (!gGame.isOn || gBoard[rowIdx][colIdx].isShown) return
@@ -154,40 +163,52 @@ function onCellMarked(elCell, event, rowIdx, colIdx) {
     } else {
         gGame.markedCount--
     }
+
     if (currCell.isMarked) {
         elCell.innerText = '🚩'
     } else {
         elCell.innerText = ''
     }
     if (gGame.shownCount === gLevel.SIZE * gLevel.SIZE - gLevel.MINES && gGame.markedCount === gLevel.MINES)
-        console.log('showcount', gGame.shownCount);
-    console.log('markedcount', gGame.markedCount);
+        //     console.log('showcount', gGame.shownCount);
+        // console.log('markedcount', gGame.markedCount);
+        if (gGame.shownCount === boardSize - gLevel.MINES - gTimesLost && gGame.markedCount + gTimesLost === gLevel.MINES || boardSize === gGame.shownCount + gGame.markedCount + gTimesLost) onWin()
     if (gGame.shownCount === 1 && gGame.markedCount === 0 || gGame.shownCount === 0 && gGame.markedCount === 1)
-        startTimer()
+        timer()
+
 
     flagCount()
-}
-
-function checkGameOver() {
 
 }
 
 function expandShown(board, elCell, i, j) {
+    if (board[i][j].isShown && elCell.innerHTML === '0') {
 
+    }
 }
 
 function onLose() {
+    gLifeCount--
     gGame.isOn = false
     var elBtn = document.querySelector('.reset')
     elBtn.innerHTML = `<button class="restart" onclick=onInit()>🤕</button>`
     stopTimer()
+
 }
 
 function onWin() {
     var elBtn = document.querySelector('.reset')
     elBtn.innerHTML = `<button class="restart" onclick=onInit()>😎</button>`
-    stopTimer()
 
+
+    // console.log('this', gTime);
+    if (gGame.secsPassed < gBestTime) {
+        gBestTime = gGame.secsPassed
+        bestTime()
+    }
+    // console.log('best', gBestTime);
+    gGame.isOn = false
+    clearInterval(gInterval)
 }
 
 function getRandomIntInclusive(min, max) {
@@ -202,7 +223,6 @@ function chooseEasy() {
     gLevel.SIZE = 4
     gLevel.MINES = 2
     onInit()
-
 }
 
 function chooseNormal() {
@@ -221,48 +241,19 @@ function chooseHard() {
     onInit()
 }
 
-function startTimer() {
-    gStartTime = new Date().getTime();
-    gTimerInterval = setInterval(updateTimerDisplay, 10);
-}
-
-function updateTimerDisplay() {
-    if (!gStartTime) return;
-
-    var currentTime = new Date().getTime();
-    var elapsedTime = currentTime - gStartTime;
-    displayTimer(elapsedTime);
-}
-
-function displayTimer(time) {
-    var minutes = Math.floor(time / 60000);
-    var seconds = Math.floor((time % 60000) / 1000);
-    var milliseconds = (time % 1000).toString().padStart(3, '0');
-
-    var timerDisplay = document.querySelector('.timer-display');
-    timerDisplay.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${milliseconds}`;
-}
-
-function stopTimer() {
-    clearInterval(gTimerInterval)
-    gStartTime = null
-}
-
 function flagCount() {
     const elFlag = document.querySelector('.flagged')
-    elFlag.innerText = `${gGame.markedCount}`
+    elFlag.innerText = `${gLevel.MINES - gGame.markedCount - gTimesLost}`
 }
 
-var Modes = true
 function darkMode() {
+    const elBody = document.querySelector('body')
+    const elAll = document.querySelector('.all')
+    const elEasy = document.querySelector('.easy')
+    const elNormal = document.querySelector('.normal')
+    const elHard = document.querySelector('.hard')
+    const elBtn = document.querySelector('.darkmode')
     if (Modes) {
-        const elBody = document.querySelector('body')
-        const elAll = document.querySelector('.all')
-        const elEasy = document.querySelector('.easy')
-        const elNormal = document.querySelector('.normal')
-        const elHard = document.querySelector('.hard')
-        const elBtn = document.querySelector('.darkmode')
-
         elBody.style.backgroundColor = 'rgb(20, 20, 20)'
         elAll.style.color = 'white'
         elEasy.style.backgroundColor = 'green'
@@ -271,13 +262,6 @@ function darkMode() {
         elBtn.innerText = 'Light Mode'
         Modes = false
     } else {
-        const elBody = document.querySelector('body')
-        const elAll = document.querySelector('.all')
-        const elEasy = document.querySelector('.easy')
-        const elNormal = document.querySelector('.normal')
-        const elHard = document.querySelector('.hard')
-        const elBtn = document.querySelector('.darkmode')
-
         elBody.style.backgroundColor = ''
         elAll.style.color = ''
         elEasy.style.backgroundColor = ''
@@ -288,5 +272,29 @@ function darkMode() {
 
     }
 
+}
+
+function bestTime() {
+    if (typeof (Storage) !== 'undefined') {
+        localStorage.setItem('bestTime', 'Best Time: ' + gBestTime + ' Seconds')
+        document.getElementById('best-time').innerHTML = localStorage.getItem('bestTime')
+    }
+}
+
+function lifeCount() {
+    var elLife = document.querySelector('.lives')
+    elLife.innerText = `${gLifeCount}`
+}
+
+function timer() {
+    gInterval = setInterval(timer2, 1000)
+}
+
+function timer2() {
+    const elDisplayTimer = document.querySelector('.timer-display')
+    if (gGame.isOn) {
+        gGame.secsPassed++
+        elDisplayTimer.innerHTML = gGame.secsPassed
+    }
 }
 
